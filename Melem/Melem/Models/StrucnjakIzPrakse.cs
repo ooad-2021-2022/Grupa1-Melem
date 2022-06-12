01a9e02a14e4517f06d91422a8c40a514e5ea8c2
Mea Hrbenic <mhrbenic1@etf.unsa.ba>
2022-06-12 20:17:01 +0200

    dodani atributi za strucnjaka iz prakse i enum tipstrucnjaka

--- Melem/Melem/Models/StrucnjakIzPrakse.cs
+++ Melem/Melem/Models/StrucnjakIzPrakse.cs
@@ -3,6 +3,11 @@
     public class StrucnjakIzPrakse
     {
         public int Id { get; set;}
-
+        private string ime { get; set;}
+        private string prezime { get; set;}
+        private string sifra { get; set;}
+        private string emailAdresa { get; set;}
+        private string slikaProfila { get; set;}
+        private TipStrucnjaka tipStrucnjaka { get; set; }
     }
 }
