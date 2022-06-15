17334024d41fc5f045783df57c70e5d12d7dafa2
Ilma Husić <ihusic1@etf.unsa.ba>
2022-06-15 21:23:29 +0200

    dodan key

--- Melem/Melem/Models/Korisnik.cs
+++ Melem/Melem/Models/Korisnik.cs
@@ -1,21 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Melem.Models
 {
     public class Korisnik
     {
+        [Key]
         public int Id { get; set; }
-        public string ime { get; set;}
-        public string prezime { get; set;}
-        public string sifra { get; set;}
-        public string emailAdresa { get; set;}
-        public string korisnickoIme {get; set;}
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string sifra { get; set; }
+        public string emailAdresa { get; set; }
+        public string korisnickoIme { get; set; }
         public DateTime datumRodjenja { get; set; }
-        public string slikaProfila { get; set;}
-        public Spol spol {get; set; }
+        public string slikaProfila { get; set; }
+        public Spol spol { get; set; }
         public TipKoze tipKoze { get; set; }
-        public int IDRutine {get; set; }
+        public int IDRutine { get; set; }
         public TipStrucnjaka tipStrucnjaka { get; set; }
-    }
+    } 
 
 }
