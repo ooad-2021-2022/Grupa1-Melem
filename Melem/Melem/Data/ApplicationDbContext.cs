6e246ef231c4053c9e10279e70f790b6e8adfecb
Mea Hrbenic <mhrbenic1@etf.unsa.ba>
2022-06-15 22:24:06 +0200

    konfiguracija za koristenje baze

--- Melem/Melem/Data/ApplicationDbContext.cs
+++ Melem/Melem/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Melem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,33 @@ namespace Melem.Data
             : base(options)
         {
         }
+
+        public DbSet<Korisnik> Korisnik { get; set; }
+        public DbSet<Kviz> Kviz { get; set; }
+        public DbSet<Rutina> Rutina { get; set; }
+        public DbSet<Produkt> Produkt { get; set; }
+        public DbSet<Sastojak> Sastojak { get; set; }
+        public DbSet<Pitanje> Pitanje{ get; set; }
+        public DbSet<Odgovor> Odgovor { get; set; }
+        public DbSet<RutinaKorisnika> RutinaKorisnika { get; set; }
+        public DbSet<SastojakIzProdukta> SastojakIzProdukta { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Korisnik>().ToTable("Korisnik");
+            modelBuilder.Entity<Kviz>().ToTable("Kviz");
+            modelBuilder.Entity<Produkt>().ToTable("Produkt");
+            modelBuilder.Entity<Rutina>().ToTable("Rutina");
+            modelBuilder.Entity<Sastojak>().ToTable("Sastojak");
+            modelBuilder.Entity<Pitanje>().ToTable("Pitanje");
+            modelBuilder.Entity<Odgovor>().ToTable("Odgovor");
+            modelBuilder.Entity<RutinaKorisnika>().ToTable("RutinaKorisnika");
+            modelBuilder.Entity<SastojakIzProdukta>().ToTable("SastojakIzProdukta");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
     }
 }
